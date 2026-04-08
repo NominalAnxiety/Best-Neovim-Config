100b840215344eadd586ac5332e08814b01e8612
Austin Bowman <astrangbow2@gmail.com>
2026-04-08 09:55:51 -0700

    changed nvim to have a transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,8 +4,8 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 	
 	-- set the background transparent
-	-- vim.api.nvim_set_hl(0,"Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0,"NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0,"Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0,"NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
