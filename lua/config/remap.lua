ed4dd549123f6f3ca99bcc7d88e5e07d6ea0f2e0
Austin Bowman <austinbowman@Austins-MacBook-Air.local>
2026-04-12 17:13:48 -0700

    added autocentering to C-u and C-d

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -25,6 +25,8 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 -- allows you to replace the word you currently have selected
 vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
